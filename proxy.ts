d4c12e866a99e7b7959e4b15d55f963a483b5be6
Lalit gurjar <gujarlalit79@gmail.com>
2026-07-06 14:57:36 +0530

    feat: add phosphor-icons dependency and implement user menu component

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,5 @@
 import type { NextRequest } from "next/server";
-// import { handleAuthProxy } from "./features/auth/utils/auth-proxy";
+import { handleAuthProxy } from "./features/auth/utils/auth-proxy";
 
 export async function proxy(request: NextRequest) {
   return handleAuthProxy(request);
